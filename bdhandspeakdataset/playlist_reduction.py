6bfcd21de40d10b86039e8bfa161ec526fe8ef2e
Mir Mahathir Mohammad <36355905+mirmahathir1@users.noreply.github.com>
2022-09-05 17:47:49 +0600

    completed reduction of videos

--- bdhandspeakdataset/playlist_reduction.py
+++ bdhandspeakdataset/playlist_reduction.py
@@ -1,16 +1,22 @@
 import reduce_one_file
 
-from os import listdir
+from os import listdir, walk
 from os.path import isfile, join
 
+path_of_reduced_videos = "reduced_videos\\"
 
-path_of_cropped_videos = "cropped_videos/"
-path_of_reduced_videos = "reduced_videos/"
-
-video_names = [f for f in listdir(path_of_cropped_videos) if isfile(join(path_of_cropped_videos, f))]
-video_names = [video for video in video_names if '.mp4' in video]
 # print(video_names)
 
+video_names = []
+
+for path, subdirs, files in walk('videos'):
+    for name in files:
+        file_path = join(path, name)  # change to your own video path
+        if '.mp4' not in file_path or 'ignore' in file_path:
+            continue
+        print(file_path)
+        video_names.append(file_path)
+
 for video_name in video_names:
-    print(path_of_cropped_videos + "/" + video_name)
-    reduce_one_file.reduce(path_of_cropped_videos, video_name, path_of_reduced_videos, 10)
+    print("" + video_name)
+    reduce_one_file.reduce('', video_name, path_of_reduced_videos, 10)
--- bdhandspeakdataset/reduce_one_file.py
+++ bdhandspeakdataset/reduce_one_file.py
@@ -9,14 +9,12 @@ def reduce(src_directory, video_file, dst_directory, fps):
         print("DUPLICATE: skipping ", dst_directory + file_name_without_ext + ".mp4")
         return
 
-    clip = VideoFileClip(src_directory + video_file)
-
-    print(clip.fps)
     # print(clip.w, " ", clip.h)
 
     print("generating: ", dst_directory + file_name_without_ext + ".mp4")
 
     clip = VideoFileClip(src_directory + video_file)
+    print("source: "+src_directory + video_file)
     clip = clip.set_fps(fps)
 
     clip.write_videofile(dst_directory + file_name_without_ext + ".mp4",
